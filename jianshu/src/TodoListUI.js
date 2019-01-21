1a163564fbceb9e35262e243da83e39cc81a05e4
Haemee <1192137406@qq.com>
2019-01-21 17:16:05 +0800

    无状态组件

--- jianshu/src/TodoListUI.js
+++ jianshu/src/TodoListUI.js
@@ -1,28 +1,26 @@
-import React , {Component} from 'react';
+import React  from 'react';
 import 'antd/dist/antd.css';
 import { Input,Button,List } from 'antd';
 
-class TodoListUI extends Component {
-    render(){
+const TodoListUI=(props)=> {
         return (
             <div>
                 <div>
                     <Input 
-                        value={this.props.inputValue} 
+                        value={props.inputValue} 
                         style={{width:'300px' ,margin:'10px 10px 0 0'}} 
-                        onChange = {this.props.handleInputChange}
+                        onChange = {props.handleInputChange}
                     />
-                    <Button type="primary" onClick={this.props.handleBtnClick}>提交</Button>
+                    <Button type="primary" onClick={props.handleBtnClick}>提交</Button>
                 </div>
                 <List 
                     style={{marginTop:'10px',width:'300px'}}
                     bordered
-                    dataSource={this.props.list}
-                    renderItem={(item,index)=>(<List.Item onClick={()=>{this.props.handleItemDelete(index)}}>{item}</List.Item>)}
+                    dataSource={props.list}
+                    renderItem={(item,index)=>(<List.Item onClick={()=>{props.handleItemDelete(index)}}>{item}</List.Item>)}
                 />
             </div>
         )
-    }
 }
 
 export default TodoListUI;
